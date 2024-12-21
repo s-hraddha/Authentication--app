3fb71625387f2b6487f58c5fc211c81c63879512
Shraddha <applicantshraddha286@gmail.com>
2024-12-21 12:26:28 +0530

    create header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,23 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Header() {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <header className='bg-black'>
+            <div className='max-w-6xl mx-auto flex justify-between items-center p-3'>
+                <Link href='/' className='text-2xl font-extrabold cursor-pointer'>
+                    <span className='text-2xl font-bold drop-shadow-md text-white'>
+                        AuthApp
+                    </span>
+                </Link>
+                <nav>
+                    <ul className='flex gap-4 text-white'>
+                        <Link href='/'>Home</Link>
+                        <Link href='/about'>About</Link>
+                        <Link href='/sign-in'>Sign In</Link>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
 }
